5e669b00dedbf3c14e6dccff618d0ae69f3a4254
vmaicon <(vmaicon.araujo@gmail.com)>
2017-03-29 23:36:02 -0300

    Implementação do método genérico para mensagens do RadioButton

--- app/src/main/java/com/example/laboratorio/ciclodevida/Q03.java
+++ app/src/main/java/com/example/laboratorio/ciclodevida/Q03.java
@@ -2,11 +2,15 @@ package com.example.laboratorio.ciclodevida;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Q03 extends AppCompatActivity {
 
@@ -22,16 +26,32 @@ public class Q03 extends AppCompatActivity {
         rgEstadoCivil = (RadioGroup) findViewById(R.id.rgEstadoCivil);
         rgEscolaridade = (RadioGroup) findViewById(R.id.rgEscolaridade);
 
+        List<RadioGroup> radioGroupList = new ArrayList<>();
+        radioGroupList.add(rgEstadoCivil);
+        radioGroupList.add(rgEscolaridade);
+
+        for(RadioGroup r : radioGroupList)
+            for(int i=0; i< r.getChildCount(); i++){
+                r.getChildAt(i).
+                        setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        selecionado(v);
+                    }
+                });
+            }
     }
 
     public void avaliar(View view){
         if(edIdade.getText().toString().equals(""))
-            mensagem("Informe a idade do canddiato");
+            mensagem("Informe a idade do candidato");
         else{
             int idade = Integer.valueOf(edIdade.getText().toString());
             if(idade > 20 &&
-                    rgEstadoCivil.getCheckedRadioButtonId() == R.id.rbSolteiro &&
-                    rgEscolaridade.getCheckedRadioButtonId() == R.id.segundograu)
+                    rgEstadoCivil.getCheckedRadioButtonId() ==
+                            R.id.rbSolteiro &&
+                    rgEscolaridade.getCheckedRadioButtonId() ==
+                            R.id.segundograu)
                 mensagem("Aprovado");
             else
                 mensagem("Não aprovado!");
@@ -40,7 +60,13 @@ public class Q03 extends AppCompatActivity {
 
     }
 
+    public void selecionado(View view) {
+        mensagem(((RadioButton)view ).
+                getText().toString() + " foi selecionado");
+    }
+
     private void mensagem(String s) {
         Toast.makeText(Q03.this, s, Toast.LENGTH_SHORT).show();
+        Log.i("App",s);
     }
 }
